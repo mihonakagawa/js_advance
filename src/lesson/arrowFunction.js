9aaa53d3708b48d45df329ddfc16408818271152
mihonakagawa <ohim212welldone@gmail.com>
2024-09-21 23:50:57 +0900

    add: アロー関数練習問題回答

--- src/lesson/arrowFunction.js
+++ src/lesson/arrowFunction.js
@@ -1,18 +1,12 @@
 // ここにコードを書きながら確認しましょう！
 
-
-
-
-
-
-
-
-
-
-
-
 // 練習問題
 // 問題 1
-const testFn = function(name) {
-  return `Gizumo ${name}`;
-};
+// const testFn = function(name) {
+//   return `Gizumo ${name}`;
+// };
+
+// 処理が1つの式で完結しているため、return文と{}が省略できる
+const testFn = name =>  `Gizumo ${name}`;
+
+console.log(testFn('太郎'));
