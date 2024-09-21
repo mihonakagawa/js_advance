fa0338f25e5673520b6c13f7aee4d314cc227772
mihonakagawa <ohim212welldone@gmail.com>
2024-09-21 23:36:32 +0900

    add: 分割代入練習問題回答

--- src/lesson/splitAssignment.js
+++ src/lesson/splitAssignment.js
@@ -1,25 +1,18 @@
 // ここにコードを書きながら確認しましょう！
 
 
-
-
-
-
-
-
-
-
-
-
 // 練習問題
 // 問題 1
 function arrayFn(arg) {
   return ['jQuery', 'Vue', arg];
 }
 
+const [valueOne, valueTwo, valueThree] = arrayFn('JS');
+console.log(valueThree);
+
 // 問題 2
-function objectFn() {
-  console.log();
+function objectFn({name}) {
+  console.log(name);
 }
 
 const argObject = {
