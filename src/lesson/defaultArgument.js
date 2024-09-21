2e0ecde1c61189f4aeee533f8dacd7c13868c324
mihonakagawa <ohim212welldone@gmail.com>
2024-09-21 23:24:23 +0900

    add: デフォルト引数練習問題回答

--- src/lesson/defaultArgument.js
+++ src/lesson/defaultArgument.js
@@ -1,18 +1,10 @@
 // ここにコードを書きながら確認しましょう！
 
-
-
-
-
-
-
-
-
-
-
-
 // 練習問題
 // 問題 1
-function discountRateCalc(price, rate) {
+function discountRateCalc(price, rate = 5) {
   return price * (1 - rate * 0.01);
 }
+
+discountRateCalc(4500);
+discountRateCalc(6000, 20);
