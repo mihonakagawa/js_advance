bfcd157e2162a38f3cfbe026f577ed66b6082c30
mihonakagawa <ohim212welldone@gmail.com>
2024-09-23 22:44:16 +0900

    add: ECMAScriptモジュール練習問題 defaultExport, defaultImport

--- src/lesson/esModules/defaultExport.js
+++ src/lesson/esModules/defaultExport.js
@@ -1,18 +1,10 @@
 // ここにコードを書きながら確認しましょう！
 
 
-
-
-
-
-
-
-
-
-
-
 // 練習問題
 // 問題 2
-function defaultFn() {
+
+// １モジュールにつき1つしかデフォルトエクスポートできない
+export default function defaultFn() {
   return 'default export';
 }
--- src/lesson/esModules/defaultImport.js
+++ src/lesson/esModules/defaultImport.js
@@ -1,15 +1,13 @@
 // ここにコードを書きながら確認しましょう！
 
 
-
-
-
-
-
-
-
-
-
-
 // 練習問題
 // 問題 2
+
+// default exportした変数や関数と命名が同じでなくても良い＝インポートする側に委ねられる。
+// named/defaultどっち使う？ → それはなぜ？
+
+// import { testFun } from './defaultExport.js' // {}は不要
+import testFun from './defaultExport.js';
+
+console.log(testFun());
